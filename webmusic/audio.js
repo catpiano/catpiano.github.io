8e001aa9f3bac7348b3362d7ff28affc4710d1f4
Neko Piano <nekopiano@users.noreply.github.com>
2015-03-09 00:01:46 +0900

    [WebMusic] Created a playground for Web audio API with a sine wave generator.

---
+++ webmusic/audio.js
@@ -0,0 +1,127 @@
+var context;
+//window.addEventListener('load', init, false);
+function init() {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    context = new AudioContext();
+  }
+  catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
+}
+init()
+
+var oscillator;
+var sounding = false;
+
+// private
+function getOscillator() {
+    if (context == undefined) {
+        console.log("now loading.");
+        return null;
+    }
+    if (oscillator == null) {
+        oscillator = context.createOscillator();
+    }
+    return oscillator;
+}
+
+function terminateOscillator() {
+    getOscillator().stop();
+    oscillator = undefined;
+    sounding = false;
+}
+
+
+// public
+function play() {
+
+    oscillator = getOscillator();
+    if (oscillator == null) {
+        console.log("now loading.");
+        return;
+    }
+
+    //var pitch = document.querySelector("#pitch")
+    oscillator.frequency.value = pitchHz;
+
+    if (sounding) {
+        console.log("still sounding.");
+        return;
+    }
+
+    oscillator.connect(context.destination);
+//    currentTime = context.currentTime;
+//    oscillator.start(currentTime);
+    oscillator.start()
+    //oscillator.stop(currentTime + 2); //stop after 2 second
+    sounding = true;
+}
+
+function changePitch(pitch) {
+    if (sounding == false) {
+      console.log("No sound yet.");
+      return;
+    }
+
+
+    oscillator = getOscillator();
+    if (oscillator == null) {
+        console.log("now loading.");
+        return;
+    }
+
+    //var pitch = document.querySelector("#pitch")
+    //console.log("pitch="+pitch)
+    oscillator.frequency.value = pitch;
+    //oscillator.start()
+
+    // oscillator.connect(context.destination);
+    // currentTime = context.currentTime;
+    // oscillator.start(currentTime);
+    // sounding = true;  
+}
+
+function stop() {
+    if (sounding == false) {
+      console.log("No sound yet.");
+      return;
+    }
+    terminateOscillator();
+}
+
+// ==== BootStrap Slider
+//https://github.com/seiyria/bootstrap-slider
+//http://www.eyecon.ro/bootstrap-slider/
+
+// Without JQuery
+var slider = new Slider("#pitch", {
+  tooltip: 'always'
+});
+
+// $('#ex1').bootstrapSlider({
+//   formatter: function(value) {
+//     return 'Current value: ' + value;
+//   }
+// });
+
+
+var pitchHz = 440;
+
+slider.on('slideStart', function(ev){
+    console.log("started to change hz=" + pitchHz)
+    pitchHz = slider.getValue();
+    changePitch(pitchHz)
+});
+slider.on('slide', function(ev){
+    console.log("changing hz=" + pitchHz)
+    pitchHz = slider.getValue();
+    changePitch(pitchHz)
+});
+slider.on('slideStop', function(ev){
+    console.log("stopped changing hz=" + pitchHz)
+    pitchHz = slider.getValue();
+    changePitch(pitchHz)
+});
+
